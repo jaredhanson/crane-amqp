1f182f541378ee5b4b749fa06f362eee3e08de7e
Jared Hanson <jaredhanson@gmail.com>
2014-03-28 11:17:20 -0700

    Update support files.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('crane-amqp', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
