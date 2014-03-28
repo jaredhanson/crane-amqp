87ee6a8e75f6a49b90354456f46a2677aa4bbf29
Jared Hanson <jaredhanson@gmail.com>
2014-03-28 11:26:37 -0700

    Test cases for package.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,33 @@
 /* global describe, it, expect */
 
-var pkg = require('..');
+var amqp = require('..');
 
 describe('crane-amqp', function() {
   
-  it('should export object', function() {
-    expect(pkg).to.be.an('object');
+  it('should export functions', function() {
+    expect(amqp.createBroker).to.be.a('function');
+  });
+  
+  it('should export constructors', function() {
+    expect(amqp.Broker).to.be.a('function');
+  });
+  
+  describe('#createBroker', function() {
+    
+    it('should create broker without message listener', function() {
+      var broker = amqp.createBroker();
+      
+      expect(broker).to.be.an.instanceOf(amqp.Broker);
+      expect(broker.listeners('message')).to.have.length(0);
+    });
+    
+    it('should create broker with message listener', function() {
+      var broker = amqp.createBroker(function(msg) {});
+      
+      expect(broker).to.be.an.instanceOf(amqp.Broker);
+      expect(broker.listeners('message')).to.have.length(1);
+    });
+    
   });
   
 });
